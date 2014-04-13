ebf606e74e1fdd773bd3b790893338d171e55d05
Theerasak Maneeneim <theerasak.maneeneim@gmail.com>
2014-04-13 13:19:28 +0700

    Add testAddBookShouldAddNewBookToShelf

--- src/Shelf.php
+++ src/Shelf.php
@@ -7,4 +7,15 @@ class Shelf {
         return count($this->bookList);
     }
     
+    public function addBook($isdn) {
+        
+        array_push($this->bookList, $isdn);
+        return $this->numberOfBooks();
+    }
+    
+    public function isOnShelf($isdn) {
+        $isInArray = in_array($isdn, $this->bookList);
+        return $isInArray;
+    }
+    
 }
--- test/ShelfTest.php
+++ test/ShelfTest.php
@@ -7,4 +7,10 @@ class ShelfTest extends PHPUnit_Framework_TestCase {
         $shelf = new Shelf();
         $this->assertEquals(0, $shelf->numberOfBooks());
     }
+    
+    function testAddBookShouldAddNewBookToShelf() {
+        $shelf = new Shelf();
+        $shelf->AddBook("9780987332103");
+        $this->assertEquals(true, $shelf->isOnShelf("9780987332103"));
+    }
 }
