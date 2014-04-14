630a848c917eec065607d1a587b4fb3f18195ccd
Theerasak Maneeneim <theerasak.maneeneim@gmail.com>
2014-04-14 11:54:22 +0700

    Refactor ShelfTest

--- test/ShelfTest.php
+++ test/ShelfTest.php
@@ -6,62 +6,59 @@ require_once "src/Book.php";
 
 class ShelfTest extends PHPUnit_Framework_TestCase {
     
+    protected  $shelf; 
+    protected  $bookCatalog;
+    protected  $book1;
+    protected  $book2;
+            
+    
+    function setUp() {
+        $this->shelf = new Shelf();
+        $this->bookCatalog = new BookCatalog();
+        $this->book1 = $this->bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");
+        $this->book2 = $this->bookCatalog->add("9780321658883","DESIGNING WITH PROGRESSIVE ENHANCEMENT","Todd Parker",2010,"New Riders");
+        $this->shelf->addBook($this->book1);
+        $this->shelf->addBook($this->book2);
+    }
+    
+    function tearDown() {
+        unset($this->book1);
+        unset($this->book2);
+        unset($this->bookCatalog);
+        unset($this->shelf);
+    }
+    
     function testNewShelfShouldReturnEmpty () {
         $shelf = new Shelf();
         $this->assertEquals(0, $shelf->numberOfBooks());
     }
     
     function testAddBookShouldAddNewBookAndReturnNumberOfBooks() {
-        $shelf = new Shelf();
-        $bookCatalog = new BookCatalog();
-        $book = $bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");
-        $this->assertEquals(1, $shelf->addBook($book));
-        $book = $bookCatalog->add("9780321658883","DESIGNING WITH PROGRESSIVE ENHANCEMENT","Todd Parker",2010,"New Riders");
-        $this->assertEquals(2, $shelf->addBook($book));
+        $book = $this->bookCatalog->add("9786163876263","The Heirs","Kim Eun-Sook",2014,"Amarin");
+        $this->assertEquals(3, $this->shelf->addBook($book));
     }
-    function testAddBookShouldAddNewBookToShelf() {
-        $shelf = new Shelf();
-        $bookCatalog = new BookCatalog();
-        $book = $bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");
-        $shelf->addBook($book);
-        $this->assertEquals(true, $shelf->isOnShelf("9780987332103"));
+    
+    function testAddBookShouldAddNewBookToShelf() {        
+        $this->shelf->addBook($this->book1);
+        $this->assertEquals(true, $this->shelf->isOnShelf("9780987332103"));
     }
     
     
     function testListAllBooksShouldReturnArray() {
-        $shelf = new Shelf();
-        $bookCatalog = new BookCatalog();
-        $book = $bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");       
-        $shelf->addBook($book);
-        $book = $bookCatalog->add("9780321658883","DESIGNING WITH PROGRESSIVE ENHANCEMENT","Todd Parker",2010,"New Riders");
-        $shelf->addBook($book);
-        $bookList = $shelf->listAllBooks();
+        $bookList = $this->shelf->listAllBooks();
         $this->assertEquals(true , is_array($bookList));
-        
     }
     
     
     function testListAllBooksShouldReturnArrayOfBook() {
-        $shelf = new Shelf();
-        $bookCatalog = new BookCatalog();
-        $book = $bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");       
-        $shelf->addBook($book);
-        $book = $bookCatalog->add("9780321658883","DESIGNING WITH PROGRESSIVE ENHANCEMENT","Todd Parker",2010,"New Riders");
-        $shelf->addBook($book);
-        $bookList = $shelf->listAllBooks();
+        $bookList = $this->shelf->listAllBooks();
         $this->assertEquals(true , ($bookList["9780987332103"] instanceof Book));
         $this->assertEquals(true , ($bookList["9780321658883"] instanceof Book));
         
     }
     
     function testGetBookShouldReturnBook() {
-        $shelf = new Shelf();
-        $bookCatalog = new BookCatalog();
-        $book = $bookCatalog->add("9780987332103","JUMP START NodeJS","Don Nguyen",2012,"sitepoint");       
-        $shelf->addBook($book);
-        $book = $bookCatalog->add("9780321658883","DESIGNING WITH PROGRESSIVE ENHANCEMENT","Todd Parker",2010,"New Riders");
-        $shelf->addBook($book);
-        $getBookResult = $shelf->getBook("9780321658883");
+        $getBookResult = $this->shelf->getBook("9780321658883");
         $this->assertEquals(true , ($getBookResult instanceof Book));
         $this->assertEquals("9780321658883", $getBookResult->getIsbn());
         $this->assertEquals("DESIGNING WITH PROGRESSIVE ENHANCEMENT", $getBookResult->getBookName());
